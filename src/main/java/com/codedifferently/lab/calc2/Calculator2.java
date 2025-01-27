730dade23ff499f49870fa3768c615af75ca97bb
Kahlilwalker <Kahlilmyson@gmail.com>
2025-01-27 13:44:06 -0500

    new work

--- src/main/java/com/codedifferently/lab/calc2/Calculator2.java
+++ src/main/java/com/codedifferently/lab/calc2/Calculator2.java
@@ -1,12 +1,14 @@
 package com.codedifferently.lab.calc2;
 
 public class Calculator2 {
-    public double tax = .05;
-    public double tip = .15;  //Change tax and tip if you prefer different values
-    public double originalPrice = 0;
+    public static double tax = .05;
+    public static double tip = .15;  //Change tax and tip if you prefer different values
+    public double originalPrice = 0.0;
 
-    public void findTotal(){
+    public static void findTotal(double price) {
         //Calculate an individual's total after tax and tip
         //Print this value
+        double total = price * (1 + tax + tip);
+        System.out.println("$" + total);
     }
 }
--- src/main/java/com/codedifferently/lab/calc2/CalculatorTest2.java
+++ src/main/java/com/codedifferently/lab/calc2/CalculatorTest2.java
@@ -5,12 +5,15 @@ import com.codedifferently.lab.calc2.Calculator2;
 public class CalculatorTest2 {
     public static void main(String[] args) {
         //Instantiate a Calculator2 object
+     Calculator2 calc = new Calculator2();
 
 
-
-        //Access the Calculator2 object's fields and methods
-        //to find the total for each member of the birthday party
-
+        /*
+        Access the Calculator2 object's fields and methods
+        to find the total for each member of the birthday party
+        */
+        calc.originalPrice= 10.0;
+        calc.findTotal(12);
 
         /*This is what everyone owes before tax and tip:
         Person 1: $10
